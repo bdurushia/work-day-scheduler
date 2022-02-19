f6eb370a10fc59e918328e40cb30e4d6f35f6735
bdurushia <b.durushia@gmail.com>
2022-02-19 12:23:50 -0600

    used moment.js and created conditionals and for loop to dynamically change color of time blocks depending whether they are past the current time, in the present time, or future time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,80 @@
-var hours = [
-    {time: "9:00am"},
-    {time: "10:00am"},
-    {time: "11:00am"},
-    {time: "12:00pm"},
-    {time: "1:00pm"},
-    {time: "2:00pm"},
-    {time: "3:00pm"},
-    {time: "4:00pm"},
-    {time: "5:00pm"}
+const hours = [
+    {time: ["09:00am", "nine-am"]},
+    {time: ["10:00am", "ten-am"]},
+    {time: ["11:00am", "eleven-am"]},
+    {time: ["12:00pm", "twelve-pm"]},
+    {time: ["01:00pm", "one-pm"]},
+    {time: ["02:00pm", "two-pm"]},
+    {time: ["03:00pm", "three-pm"]},
+    {time: ["04:00pm", "four-pm"]},
+    {time: ["05:00pm", "five-pm"]}
 ];
 
+// Display Current Date using Moment.js
+const todaysDate = moment().format('MMMM Do, YYYY');
+$('#currentDay').text(todaysDate);
+
 // Create time Block
-var createTimeBlock = function() {
+const createTimeBlock = function() {
+    const blockContainer = $('.container');
     
+    $.each(hours, function(i, time) {
+        
+        let ulEl = $('<ul>')
+        .addClass('list-group list-group-horizontal');
+        
+        let hourLi = $('<li>')
+        .addClass('list-group-item hour ' + hours[i].time[1])
+        .text(hours[i].time[0]);
+        
+        let timeBlockLi = $('<li>')
+        .prop('id', hours[i].time[1])
+        .addClass('list-group-item time-block container-fluid');
+        
+        let textAreaEl = $('<textarea>')
+        .addClass('container-fluid');
+        
+        let saveIcon = $('<i>')
+        .addClass('fas fa-floppy-disk');
+
+        let spanEl = $('<span>');
+
+        let saveBtnLi = $('<li>')
+        .addClass('list-group-item saveBtn')
+        .append($('<button type=button">').append(spanEl.append(saveIcon)));
+        
+        blockContainer
+            .append(ulEl
+                .append(hourLi, 
+                    timeBlockLi.append(textAreaEl), 
+                    saveBtnLi)
+            );
+    });
+};
+
+const checkHour = function() {
+    let currentHour = moment().format("hh");
+    let amPm = moment().format("a");
+    
+    $.each(hours, function(i, time) {
+        let hourAttr = hours[i].time[1];
+        let timeBlock = $('#' + hourAttr);
+        let timeTextArr = $('.' + hourAttr).text().split('');
+        let textAmPm = timeTextArr[5] + timeTextArr[6];
+        let textHour = timeTextArr[0] + timeTextArr[1];
+
+        if (textHour <= currentHour && textAmPm !== amPm) {
+            // example 09 <= 12 && am !== pm
+            // example 01 <= 11 && pm !== am
+            timeBlock.addClass('past');
+        } else if (textHour === currentHour) {
+            timeBlock.addClass('present');
+        } else {
+            timeBlock.addClass('future');
+        }
+        
+    });
 };
 
 createTimeBlock();
-// style .time-block not wokring
+checkHour();
